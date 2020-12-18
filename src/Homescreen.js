dd69081bfac0cc2ce057a8210183ab9368e6cc1c
vijaya8993 <vijaya@vtcgroups.com>
2020-12-17 23:37:18 -0500

    added router example

---
+++ src/Homescreen.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class HomeScreen extends Component{
+
+
+    render() {
+
+        return (
+            <div  >
+                <h1>Hi router example</h1>
+            </div>
+
+        );
+    }}
+export default HomeScreen;
